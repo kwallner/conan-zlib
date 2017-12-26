46b1a40ad7a0b8db7dc6eb39ef42f0d9757e46ac
Karl Wallner <karl.wallner@gmx.de>
2017-12-26 10:16:01 +0100

    Updated user names. Testing on appveyor.

--- conanfile.py
+++ conanfile.py
@@ -13,7 +13,7 @@ class ZlibConan(ConanFile):
     options = {"shared": [True, False]}
     default_options = "shared=False"
     exports_sources = ["CMakeLists.txt"]
-    url = "http://github.com/lasote/conan-zlib"
+    url = "http://github.com/kwallner/conan-zlib"
     license = "http://www.zlib.net/zlib_license.html"
     description = "A Massively Spiffy Yet Delicately Unobtrusive Compression Library " \
                   "(Also Free, Not to Mention Unencumbered by Patents)"
