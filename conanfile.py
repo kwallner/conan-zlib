86c7cf94443f91fd1321bf498002aa16a4400161
Karl Wallner <karl.wallner@gmx.de>
2018-01-02 16:26:52 +0100

    Using cmake also on linux. Naming conforming to FindZlib.

--- conanfile.py
+++ conanfile.py
@@ -1,4 +1,4 @@
-from conans import ConanFile, tools, CMake, AutoToolsBuildEnvironment
+from conans import ConanFile, tools, CMake
 from conans.util import files
 from conans import __version__ as conan_version
 import os
@@ -12,7 +12,6 @@ class ZlibConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
     options = {"shared": [True, False], "fPIC": [True, False]}
     default_options = "shared=False", "fPIC=True"
-    exports_sources = ["CMakeLists.txt"]
     url = "http://github.com/kwallner/conan-zlib"
     license = "http://www.zlib.net/zlib_license.html"
     description = "A Massively Spiffy Yet Delicately Unobtrusive Compression Library " \
@@ -29,42 +28,40 @@ class ZlibConan(ConanFile):
         files.rmdir("%s/contrib" % self.ZIP_FOLDER_NAME)
         if self.settings.os != "Windows":
             self.run("chmod +x ./%s/configure" % self.ZIP_FOLDER_NAME)
-
+          
+        # Conanize
+        tools.replace_in_file("%s/CMakeLists.txt" % self.ZIP_FOLDER_NAME, 'project(zlib C)', '''
+project(zlib C)
+include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
+CONAN_BASIC_SETUP()
+''')
+         
+        # Fix to build either shared or static
+        tools.replace_in_file("%s/CMakeLists.txt" % self.ZIP_FOLDER_NAME, 'add_library(zlib SHARED ${ZLIB_SRCS} ${ZLIB_ASMS} ${ZLIB_DLL_SRCS} ${ZLIB_PUBLIC_HDRS} ${ZLIB_PRIVATE_HDRS})', '''
+if (BUILD_SHARED_LIBS)
+add_library(zlib SHARED ${ZLIB_SRCS} ${ZLIB_ASMS} ${ZLIB_DLL_SRCS} ${ZLIB_PUBLIC_HDRS} ${ZLIB_PRIVATE_HDRS})
+endif()                            
+''')
+        tools.replace_in_file("%s/CMakeLists.txt" % self.ZIP_FOLDER_NAME, 'set_target_properties(zlib PROPERTIES DEFINE_SYMBOL ZLIB_DLL)', '''
+if (BUILD_SHARED_LIBS)
+set_target_properties(zlib PROPERTIES DEFINE_SYMBOL ZLIB_DLL)
+endif()                            
+''')
+        tools.replace_in_file("%s/CMakeLists.txt" % self.ZIP_FOLDER_NAME, 'add_library(zlibstatic STATIC ${ZLIB_SRCS} ${ZLIB_ASMS} ${ZLIB_PUBLIC_HDRS} ${ZLIB_PRIVATE_HDRS})', '''
+if (NOT BUILD_SHARED_LIBS)
+add_library(zlib STATIC ${ZLIB_SRCS} ${ZLIB_ASMS} ${ZLIB_PUBLIC_HDRS} ${ZLIB_PRIVATE_HDRS})
+endif()                            
+''')
+        tools.replace_in_file("%s/CMakeLists.txt" % self.ZIP_FOLDER_NAME, 'zlib zlibstatic', 'zlib')
+                              
     def build(self):
-        with tools.chdir(self.ZIP_FOLDER_NAME):
-            files.mkdir("_build")
-            with tools.chdir("_build"):
-                if not tools.os_info.is_windows:
-                    env_build = AutoToolsBuildEnvironment(self)
-                    if self.settings.arch in ["x86", "x86_64"] and self.settings.compiler in ["apple-clang", "clang", "gcc"]:
-                        env_build.flags.append('-mstackrealign')
-
-                    env_build.fPIC = self.options.shared or self.options.fPIC
-                    
-                    # env_build.fPIC seems not to pass the fPIC options
-                    compiler_flags = []
-                    if env_build.fPIC:
-                        compiler_flags.append("-fPIC")
-                    
-                    if self.settings.os == "Macos":
-                        old_str = '-install_name $libdir/$SHAREDLIBM'
-                        new_str = '-install_name $SHAREDLIBM'
-                        tools.replace_in_file("../configure", old_str, new_str)
-
-                    args= []
-                    if compiler_flags:  
-                        args.append("CFLAGS='%s'" % " ".join(compiler_flags))
-                        args.append("LDFLAGS='%s'" % " ".join(compiler_flags))
-                        
-                    # Zlib configure doesnt allow this parameters
-                    env_build.configure("../", build=False, host=False, target=False, args=args)
-                    env_build.make()
-                else:
-                    cmake = CMake(self)
-                    cmake.definitions["CMAKE_POSITION_INDEPENDENT_CODE"] = self.options.shared or self.options.fPIC
-                    cmake.configure(build_dir=".")
-                    cmake.build(build_dir=".")
-
+        cmake = CMake(self)
+        cmake.definitions["CMAKE_POSITION_INDEPENDENT_CODE"] = "ON" if self.options.shared or self.options.fPIC else "OFF"
+        cmake.definitions["BUILD_SHARED_LIBS"] = "ON" if self.options.shared else "OFF"
+        cmake.configure(source_dir="%s/%s" % (self.source_folder, self.ZIP_FOLDER_NAME))
+        cmake.build()
+        cmake.install()
+        
     def package(self):
         # Extract the License/s from the header to a file
         with tools.chdir(os.path.join(self.build_folder, self.ZIP_FOLDER_NAME)):
@@ -74,48 +71,6 @@ class ZlibConan(ConanFile):
 
         # Copy the license files
         self.copy("LICENSE", src=self.ZIP_FOLDER_NAME, dst=".")
-
-        # Copy pc file
-        self.copy("*.pc", dst="", keep_path=False)
-        # Copying zlib.h, zutil.h, zconf.h
-        self.copy("*.h", "include", "%s" % self.ZIP_FOLDER_NAME, keep_path=False)
-        self.copy("*.h", "include", "%s" % "_build", keep_path=False)
-
-        # Copying static and dynamic libs
-        build_dir = os.path.join(self.ZIP_FOLDER_NAME, "_build")
-        if tools.os_info.is_windows:
-            if self.options.shared:
-                build_dir = os.path.join(self.ZIP_FOLDER_NAME, "_build")
-                self.copy(pattern="*.dll", dst="bin", src=build_dir, keep_path=False)
-                build_dir = os.path.join(self.ZIP_FOLDER_NAME, "_build/lib")
-                self.copy(pattern="*zlibd.lib", dst="lib", src=build_dir, keep_path=False)
-                self.copy(pattern="*zlib.lib", dst="lib", src=build_dir, keep_path=False)
-                self.copy(pattern="*zlib.dll.a", dst="lib", src=build_dir, keep_path=False)
-            else:
-                build_dir = os.path.join(self.ZIP_FOLDER_NAME, "_build/lib")
-                # MinGW
-                self.copy(pattern="libzlibstaticd.a", dst="lib", src=build_dir, keep_path=False)
-                self.copy(pattern="libzlibstatic.a", dst="lib", src=build_dir, keep_path=False)
-                # Visual Studio
-                self.copy(pattern="zlibstaticd.lib", dst="lib", src=build_dir, keep_path=False)
-                self.copy(pattern="zlibstatic.lib", dst="lib", src=build_dir, keep_path=False)
-
-                lib_path = os.path.join(self.package_folder, "lib")
-                suffix = "d" if self.settings.build_type == "Debug" else ""
-                if self.settings.compiler == "Visual Studio":
-                    current_lib = os.path.join(lib_path, "zlibstatic%s.lib" % suffix)
-                    os.rename(current_lib, os.path.join(lib_path, "zlib%s.lib" % suffix))
-                elif self.settings.compiler == "gcc":
-                    current_lib = os.path.join(lib_path, "libzlibstatic.a")
-                    os.rename(current_lib, os.path.join(lib_path, "libzlib.a"))
-        else:
-            if self.options.shared:
-                if self.settings.os == "Macos":
-                    self.copy(pattern="*.dylib", dst="lib", src=build_dir, keep_path=False)
-                else:
-                    self.copy(pattern="*.so*", dst="lib", src=build_dir, keep_path=False)
-            else:
-                self.copy(pattern="*.a", dst="lib", src=build_dir, keep_path=False)
 
     def package_info(self):
         if self.settings.os == "Windows":
